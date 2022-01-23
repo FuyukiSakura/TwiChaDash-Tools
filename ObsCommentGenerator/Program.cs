43fa1c7083c71f11dc71d6e70d582b964d01fe01
Fuyuki Sakura <fuyuki@sakura.live>
2022-01-23 22:05:27 +1000

    initial commit

---
+++ ObsCommentGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using ObsCommentGenerator.Models;
+
+namespace ObsCommentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
