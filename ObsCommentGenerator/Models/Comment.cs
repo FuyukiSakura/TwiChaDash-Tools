d3aede92c26d8689f5048d13b97332e7ad58ef4d
Fuyuki Sakura <fuyuki@sakura.live>
2022-01-23 22:13:44 +1000

    read TwiChaDash html output

---
+++ ObsCommentGenerator/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ObsCommentGenerator.Models
+{
+    public class Comment
+    {
+        public string No { get; set; }
+
+        public string Message { get; set; }
+    }
+}
--- ObsCommentGenerator/Program.cs
+++ ObsCommentGenerator/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Xml;
 using System.Xml.Linq;
-using System.Xml.Serialization;
 using ObsCommentGenerator.Models;
 
 namespace ObsCommentGenerator
@@ -13,7 +10,31 @@ namespace ObsCommentGenerator
     {
         static void Main(string[] args)
         {
+            var comments = GetMyComments(args[0]);
+            Console.WriteLine(string.Join("\r\n", comments.Select(cm => cm.Message)));
+            Console.ReadLine();
+        }
 
+        /// <summary>
+        /// Get all of my comments from TTS
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        static IEnumerable<Comment> GetMyComments(string filename)
+        {
+            // load xml file
+            var xml = XDocument.Load(filename);
+
+            var log = xml.Root?.Descendants("comment");
+            var comments = log?
+                .Where(comment => comment.Attribute("service")!.Value == ""
+                                  && comment.Attribute("handle")!.Value == "")
+                .Select(comment => new Comment
+                {
+                    No = comment.Attribute("No")!.Value,
+                    Message = comment.Value
+                });
+            return comments;
         }
     }
 }
